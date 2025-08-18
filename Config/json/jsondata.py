d9139bdaa609c6ca793623ac09f50bb4f4d33aea
Antonio Tanco <antonio.tanco1999@gmail.com>
2025-08-17 22:24:14 -0400

    Implemented JSON Logging feature

---
+++ Config/json/jsondata.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from datetime import time
+from typing import List
+
+# Using dataclasses to define the JSON Log Structure
+
+@dataclass
+class Jsonlog:
+    hostname: str
+    targeted_services: List[str]
+    targeted_paths: List[str]
+    hashes_generated: List[str]
