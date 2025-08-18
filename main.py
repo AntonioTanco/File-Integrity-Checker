43dec9813b960dcc16ebe2901751fef190a0c1b5
Antonio Tanco <antonio.tanco1999@gmail.com>
2025-08-18 13:07:55 -0400

    commented out all functions in main.py to work on the yaml config structure

--- main.py
+++ main.py
@@ -7,32 +7,34 @@ from Utils.system import get_nodes_name
 import Utils.system.check_if_file_exist as system
 
 # use getServicesLogPaths() from config to store a list of paths to files from CONFIG.YAML
-TARGETED_LOG_FILE = config.getServicesLogPaths()
+# TARGETED_LOG_FILE = config.getServicesLogPaths()
 
 # use getServicesName() from config to store a list of paths to files from CONFIG.YAML
-TARGETED_SERVICE_NAMES = config.getServicesName()
+# TARGETED_SERVICE_NAMES = config.getServicesName()
 
 # run check on TARGETED_LOG_FILE to see if files exist within the system
-file_path_exist = system.check_if_file_exist(TARGETED_LOG_FILE)
+# file_path_exist = system.check_if_file_exist(TARGETED_LOG_FILE)
 
 # Creating function to run hash computation on program execution
 def run_hash_computation():
 
     # Checking if files listed in YAML Config exist within the system
-    if file_path_exist == True:
+    # if file_path_exist == True:
         
-        # calculate and return hash of all the files present in YAML Config
-        cal_hash = hash.getFilesHash(TARGETED_LOG_FILE)
+    #     # calculate and return hash of all the files present in YAML Config
+    #     cal_hash = hash.getFilesHash(TARGETED_LOG_FILE)
 
-        operations = _json.Jsonlog(hostname=get_nodes_name(), 
-                                   targeted_paths=TARGETED_LOG_FILE, 
-                                   targeted_services=TARGETED_SERVICE_NAMES, 
-                                   hashes_generated=cal_hash)
+    #     operations = _json.Jsonlog(hostname=get_nodes_name(), 
+    #                                targeted_paths=TARGETED_LOG_FILE, 
+    #                                targeted_services=TARGETED_SERVICE_NAMES, 
+    #                                hashes_generated=cal_hash)
 
-        log.write_to_json(operations)
+    #     log.write_to_json(operations)
+
+    print("test")
 
         # print to console - hashes were successfully calculated for the targeted services after hashes were calculated
-        syslog.logging.info(f"Calculated hashes successfully for: {TARGETED_SERVICE_NAMES}")
+        # syslog.logging.info(f"Calculated hashes successfully for: {TARGETED_SERVICE_NAMES}")
 
 if __name__ == "__main__":
     run_hash_computation()
