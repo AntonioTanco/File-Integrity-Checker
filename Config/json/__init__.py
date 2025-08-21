c495dff8aefa751f26571377a22648efba5bda05
Antonio Tanco <antonio.tanco1999@gmail.com>
2025-08-20 23:50:52 -0400

    refactored static var in __init__.py for better naming convention

--- Config/json/__init__.py
+++ Config/json/__init__.py
@@ -2,15 +2,15 @@ from pathlib import Path
 import json as json
 
 # Create a mew folder in the defined directory
-json_log_directory = Path(r"C:\Users\smoke\Documents\File Integrity Checker\Logs")
+JSON_LOG_DIRECTORY = Path(r"C:\Users\smoke\Documents\File Integrity Checker\Logs")
 
 # Creating .JSON log file 
 jSON_LOG_FILENAME = "Integrity_logs.json"
 
 # Use mkdir function to create a new folder 
-json_log_directory.mkdir(exist_ok=True)
+JSON_LOG_DIRECTORY.mkdir(exist_ok=True)
 
-json_log_filepath = json_log_directory / jSON_LOG_FILENAME
+json_log_filepath = JSON_LOG_DIRECTORY / jSON_LOG_FILENAME
 
 if not json_log_filepath.exists():
 
