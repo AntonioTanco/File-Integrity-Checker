a105fd2542e54340b632591279f28ab79f52c2b8
Antonio Tanco <antonio.tanco1999@gmail.com>
2025-12-27 18:33:49 -0500

    created start_backup func to handle backup operations

--- Utils/system/system_backup.py
+++ Utils/system/system_backup.py
@@ -1,17 +1,35 @@
 from pathlib import Path
 from Utils.logs import logging
+from Config.configops import readYamlConfig
+import yaml
 
 class SystemBackUp():
 
-    def __init__(self):
-        pass
+    def __init__(self, enabled: bool):
+        self.enabled = enabled
 
-    def start_backup(self, config_path: str):
+    def start_backup(self, config_path: str, destination_path: str ):
 
-        _config_file = Path(config_path).exists()
+        # current_config_name = Path(config_path).name
+        backup_str = destination_path + "backup-config.yaml"
 
-        if _config_file == True:
+        backup_config_path = Path(backup_str)
 
-            logging.info("Initating Backup of config.yaml")
+        logging.info(f"this path is: {backup_config_path}")
 
-            return {"Congig.yaml was found and backed up."}
+        try:
+            with backup_config_path.open('w') as backup_file:
+
+                oldConfigData = readYamlConfig()
+
+                yaml.safe_dump(oldConfigData, backup_file)
+
+                logging.info(f"{backup_file.name} was created.")
+
+                backup_file.close
+                
+        except Exception as e:
+
+            # Print to console the Exception
+            print(f"An unexpected error occurred while creating config.yaml: {e}")
+        
